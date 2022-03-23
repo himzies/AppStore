a73bf0b6ca4563867e41fcac437159ff158a168f
himzies <100895032+himzies@users.noreply.github.com>
2022-03-24 06:55:33 +0800

    Update AlfredoClean.sql

--- sql/AlfredoClean.sql
+++ sql/AlfredoClean.sql
@@ -5,6 +5,7 @@
 *******************/
 
 DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS provider;
 DROP TABLE IF EXISTS job_category;
 DROP TABLE IF EXISTS pet_care;
 DROP TABLE IF EXISTS cleaning;
