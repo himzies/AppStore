2236be0dcefd6eefc172cbc081015127e8e8da25
himzies <100895032+himzies@users.noreply.github.com>
2022-03-24 17:13:08 +0800

    Update AlfredoJobCategories.sql

--- sql/AlfredoJobCategories.sql
+++ sql/AlfredoJobCategories.sql
@@ -1,3 +1,3 @@
-INSERT INTO job_category VALUES ('Housekeeping');
+INSERT INTO job_category VALUES ('Cleaning');
 INSERT INTO job_category VALUES ('Tuition');
 INSERT INTO job_category VALUES ('Pet Care');
