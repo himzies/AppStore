79bc4bf308bc9dc03e1559ea770e65490aff95df
himzies <100895032+himzies@users.noreply.github.com>
2022-03-24 22:19:39 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('login', app.views.login, name='login'),
     path('<str:id>/services', app.views.services, name='services'),
     path('<str:id>/services/<str:service>', app.views.job_cat, name='job_cat'),
-    path('<str:id>/services/<str:service>/<str:expertise>', app.views.job_req, name='job_req')
+    path('<str:id>/services/<str:expertise>', app.views.job_req, name='job_req')
 ]
