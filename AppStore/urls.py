f84359e51d90d652904258c61a205dd59d6508cb
nicolesee01 <69748209+nicolesee01@users.noreply.github.com>
2022-03-29 17:51:44 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('database', app.views.database, name='database'),
     path('add', app.views.add, name='add'),
+    path('add_provider', app.views.add_provider, name='add_provider'),
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
     path('', app.views.home, name='home'),
