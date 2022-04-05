51de239950f3d4dd36a4abc6781531128a387494
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 01:15:44 +0800

    Delete CleaningJobs.sql

--- sql/CleaningJobs.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cleaning VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.', '40');
-INSERT INTO cleaning VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.', '20');
-INSERT INTO cleaning VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.', '30');
