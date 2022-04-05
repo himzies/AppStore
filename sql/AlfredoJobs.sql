dcbeed344c481b6948e1b1db842b1e896d3b495d
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 04:23:53 +0800

    Update AlfredoJobs.sql

--- sql/AlfredoJobs.sql
+++ sql/AlfredoJobs.sql
@@ -1,13 +1,13 @@
 
-INSERT INTO jobs VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.', '40', 'Cleaning');
-INSERT INTO jobs VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.', '20','Cleaning');
-INSERT INTO jobs VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.', '30', 'Cleaning');
-INSERT INTO jobs VALUES ('English', 'English Language tuition for all levels.', '40', 'Tuition');
-INSERT INTO jobs VALUES ('Math', 'Mathematics tuition for all levels', '50', 'Tuition');
-INSERT INTO jobs VALUES ('Chinese', 'Chinese Language tuition for all levels.', '40', 'Tuition');
-INSERT INTO jobs VALUES ('Malay', 'Malay Language tuition for all levels.', '40', 'Tuition');
-INSERT INTO jobs VALUES ('Tamil', 'Tamil Language tuition for all levels.', '40', 'Tuition');
-INSERT INTO jobs VALUES ('Science', 'Science tuition for all levels and all specialisations (Biology, Chemistry, Physics)', '50', 'Tuition');
-INSERT INTO jobs VALUES ('Pet Grooming', 'Styling of pet fur, bathing services and trimming of nails.', '60', 'Pet Care');
-INSERT INTO jobs VALUES ('Walk in the Park', 'We bring your pets for a walk in the park and letting them play with other pets.', '10', 'Pet Care');
-INSERT INTO jobs VALUES ('Petsitting', 'We take care of your pet for a certain duration while you are busy.', '20', 'Pet Care');
+INSERT INTO jobs VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.', 40, 'Cleaning');
+INSERT INTO jobs VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.', 20,'Cleaning');
+INSERT INTO jobs VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.', 30, 'Cleaning');
+INSERT INTO jobs VALUES ('English', 'English Language tuition for all levels.', 40, 'Tuition');
+INSERT INTO jobs VALUES ('Math', 'Mathematics tuition for all levels', 50, 'Tuition');
+INSERT INTO jobs VALUES ('Chinese', 'Chinese Language tuition for all levels.', 40, 'Tuition');
+INSERT INTO jobs VALUES ('Malay', 'Malay Language tuition for all levels.', 40, 'Tuition');
+INSERT INTO jobs VALUES ('Tamil', 'Tamil Language tuition for all levels.', 40, 'Tuition');
+INSERT INTO jobs VALUES ('Science', 'Science tuition for all levels and all specialisations (Biology, Chemistry, Physics)', 50, 'Tuition');
+INSERT INTO jobs VALUES ('Pet Grooming', 'Styling of pet fur, bathing services and trimming of nails.', 60, 'Pet Care');
+INSERT INTO jobs VALUES ('Walk in the Park', 'We bring your pets for a walk in the park and letting them play with other pets.', 10, 'Pet Care');
+INSERT INTO jobs VALUES ('Petsitting', 'We take care of your pet for a certain duration while you are busy.', 20, 'Pet Care');
