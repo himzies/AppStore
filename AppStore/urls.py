8463761e66560b2fd5dd4cedb2b21aff2d669a07
himzies <100895032+himzies@users.noreply.github.com>
2022-04-05 23:08:58 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
     path('tuition', app.views.tuition, name='tuition'),
     path('petcare', app.views.petcare, name='petcare'),
     #Testing for transaction
-    path('<str:id>/transaction', app.views.transaction, name='transaction')
+    path('<str:id>/job_cat/<str:service>/job_req/<str:expertise>/<str:prov_id', app.views.transaction, name='transaction')
 ]
