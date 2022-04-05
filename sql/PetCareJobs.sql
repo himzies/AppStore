0fbdf229ba4456b28b0ec780aba7201ee44b1d5b
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 01:15:38 +0800

    Delete PetCareJobs.sql

--- sql/PetCareJobs.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO pet_care VALUES ('Pet Grooming', 'Styling of pet fur, bathing services and trimming of nails.', '60');
-INSERT INTO pet_care VALUES ('Walk in the Park', 'We bring your pets for a walk in the park and letting them play with other pets.', '10');
-INSERT INTO pet_care VALUES ('Petsitting', 'We take care of your pet for a certain duration while you are busy.', '20');
