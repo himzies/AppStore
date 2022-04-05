bcaddb275b1fb8e546b14213516da7196f9437d1
himzies <100895032+himzies@users.noreply.github.com>
2022-04-05 10:44:30 +0800

    Update TuitionJobs.sql

--- sql/TuitionJobs.sql
+++ sql/TuitionJobs.sql
@@ -1,4 +1,6 @@
-INSERT INTO tuition VALUES ('English');
-INSERT INTO tuition VALUES ('Math');
-INSERT INTO tuition VALUES ('Chinese');
-INSERT INTO tuition VALUES ('Science');
+INSERT INTO tuition VALUES ('English', 'English Language tuition for all levels.', '40');
+INSERT INTO tuition VALUES ('Math', 'Mathematics tuition for all levels', '50');
+INSERT INTO tuition VALUES ('Chinese', 'Chinese Language tuition for all levels.', '40');
+INSERT INTO tuition VALUES ('Malay', 'Malay Language tuition for all levels.', '40');
+INSERT INTO tuition VALUES ('Tamil', 'Tamil Language tuition for all levels.', '40');
+INSERT INTO tuition VALUES ('Science', 'Science tuition for all levels and all specialisations (Biology, Chemistry, Physics)', '50');
