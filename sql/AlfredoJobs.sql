a138078a63af6ffccdf4a10dd83bd625695bfe6e
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 00:55:19 +0800

    Create AlfredoJobs.sql

---
+++ sql/AlfredoJobs.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO cleaning VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.', '40', 'Cleaning);
+INSERT INTO cleaning VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.', '20','Cleaning);
+INSERT INTO cleaning VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.', '30', 'Cleaning');
+INSERT INTO tuition VALUES ('English', 'English Language tuition for all levels.', '40', 'Tuition');
+INSERT INTO tuition VALUES ('Math', 'Mathematics tuition for all levels', '50', 'Tuition');
+INSERT INTO tuition VALUES ('Chinese', 'Chinese Language tuition for all levels.', '40', 'Tuition');
+INSERT INTO tuition VALUES ('Malay', 'Malay Language tuition for all levels.', '40', 'Tuition');
+INSERT INTO tuition VALUES ('Tamil', 'Tamil Language tuition for all levels.', '40', 'Tuition');
+INSERT INTO tuition VALUES ('Science', 'Science tuition for all levels and all specialisations (Biology, Chemistry, Physics)', '50', 'Tuition');
+INSERT INTO pet_care VALUES ('Pet Grooming', 'Styling of pet fur, bathing services and trimming of nails.', '60', 'Pet Care');
+INSERT INTO pet_care VALUES ('Walk in the Park', 'We bring your pets for a walk in the park and letting them play with other pets.', '10', 'Pet Care');
+INSERT INTO pet_care VALUES ('Petsitting', 'We take care of your pet for a certain duration while you are busy.', '20', 'Pet Care');
