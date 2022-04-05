ca256c0b440d6f8155295cb9dd72711a43c8af19
himzies <100895032+himzies@users.noreply.github.com>
2022-04-05 21:59:23 +0800

    Update AlfredoSchema.sql

--- sql/AlfredoSchema.sql
+++ sql/AlfredoSchema.sql
@@ -4,17 +4,8 @@
 
 ********************/
 
-CREATE TABLE IF NOT EXISTS user (
-	id VARCHAR(16) PRIMARY KEY,
-	first_name VARCHAR(64) NOT NULL,
-	last_name VARCHAR(64) NOT NULL,
-	gender VARCHAR(6) NOT NULL CHECK (gender IN ('Male', 'Female')),
-	email VARCHAR(64) UNIQUE NOT NULL);
-
 CREATE TABLE IF NOT EXISTS customer (
-	id VARCHAR(16) REFERENCES user (id)
-		ON UPDATE CASCADE ON DELETE CASCADE
-		DEFERRABLE INITIALLY DEFERRED,
+	id VARCHAR(16),
 	password VARCHAR(64) NOT NULL,
 	first_name VARCHAR(64) NOT NULL,
 	last_name VARCHAR(64) NOT NULL,
@@ -24,9 +15,7 @@ CREATE TABLE IF NOT EXISTS customer (
 	PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS provider (
-	id VARCHAR(16) REFERENCES user (id)
-		ON UPDATE CASCADE ON DELETE CASCADE
-		DEFERRABLE INITIALLY DEFERRED,
+	id VARCHAR(16),
 	password VARCHAR(64) NOT NULL,
 	first_name VARCHAR(64) NOT NULL,
 	last_name VARCHAR(64) NOT NULL,
@@ -55,10 +44,8 @@ CREATE TABLE IF NOT EXISTS tuition (
 	price DEC(16, 2) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS transaction (
-	order_id int NOT NULL PRIMARY KEY,
-	customer_id VARCHAR(16),
-	provider_id VARCHAR(16),
+	customer_id VARCHAR(16) REFERENCES customer(id),
+	provider_id VARCHAR(16) REFERENCES provider(id),
 	cust_address VARCHAR(255) UNIQUE NOT NULL,
 	expertise VARCHAR(64) NOT NULL, 
-	price DEC(16, 2) NOT NULL
-	FOREIGN KEY (customer_id, provider_id) REFERENCES user(id));
+	price DEC(16, 2) NOT NULL);
