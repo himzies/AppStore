9e31ecbe3f4f10e587188bbd5a584d8b4453255f
himzies <100895032+himzies@users.noreply.github.com>
2022-04-05 10:39:05 +0800

    Update CleaningJobs.sql

--- sql/CleaningJobs.sql
+++ sql/CleaningJobs.sql
@@ -1,3 +1,3 @@
-INSERT INTO cleaning VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.');
-INSERT INTO cleaning VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.');
-INSERT INTO cleaning VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.');
+INSERT INTO cleaning VALUES ('Housekeeping', 'Basic jobs such as vaccuming, mopping and toilet cleaning.', '40');
+INSERT INTO cleaning VALUES ('Car Washing', 'Scrubbing of exterior surfaces and cleaning of floor carpets.', '20');
+INSERT INTO cleaning VALUES ('Gardening', 'Tending to plants and flowers, removing weeds and relocating plants.', '30');
