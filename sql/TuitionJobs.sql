80eba826f4b33f29cd00423d2782de33fe039cde
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 01:15:30 +0800

    Delete TuitionJobs.sql

--- sql/TuitionJobs.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tuition VALUES ('English', 'English Language tuition for all levels.', '40');
-INSERT INTO tuition VALUES ('Math', 'Mathematics tuition for all levels', '50');
-INSERT INTO tuition VALUES ('Chinese', 'Chinese Language tuition for all levels.', '40');
-INSERT INTO tuition VALUES ('Malay', 'Malay Language tuition for all levels.', '40');
-INSERT INTO tuition VALUES ('Tamil', 'Tamil Language tuition for all levels.', '40');
-INSERT INTO tuition VALUES ('Science', 'Science tuition for all levels and all specialisations (Biology, Chemistry, Physics)', '50');
