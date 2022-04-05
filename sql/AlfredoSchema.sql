345b94824aa7e03b30c077ef92bbbe1aef9f49db
himzies <100895032+himzies@users.noreply.github.com>
2022-04-05 10:37:13 +0800

    Update AlfredoSchema.sql

--- sql/AlfredoSchema.sql
+++ sql/AlfredoSchema.sql
@@ -14,35 +14,36 @@ CREATE TABLE IF NOT EXISTS customer (
  address VARCHAR(255) UNIQUE NOT NULL);
  
 CREATE TABLE IF NOT EXISTS provider (
- id VARCHAR(16) PRIMARY KEY,
- password VARCHAR(64) NOT NULL,
- first_name VARCHAR(64) NOT NULL,
- last_name VARCHAR(64) NOT NULL,
- gender VARCHAR(6) NOT NULL CHECK (gender IN ('Male', 'Female')),
- email VARCHAR(64) UNIQUE NOT NULL,
- expertise VARCHAR(64) NOT NULL,
- address VARCHAR(255) UNIQUE NOT NULL);
- 
- CREATE TABLE IF NOT EXISTS job_category(
-  name VARCHAR(64) PRIMARY KEY);
- 
- CREATE TABLE IF NOT EXISTS cleaning(
-  name VARCHAR(64) PRIMARY KEY,
-  descrip VARCHAR(255));
- 
- CREATE TABLE IF NOT EXISTS pet_care(
-  name VARCHAR(64) PRIMARY KEY,
-  descrip VARCHAR(255));
- 
- CREATE TABLE IF NOT EXISTS tuition(
-  name VARCHAR(64) PRIMARY KEY,
-  descrip VARCHAR(255));
- 
+	id VARCHAR(16) PRIMARY KEY,
+	password VARCHAR(64) NOT NULL,
+	first_name VARCHAR(64) NOT NULL,
+	last_name VARCHAR(64) NOT NULL,
+	gender VARCHAR(6) NOT NULL CHECK (gender IN ('Male', 'Female')),
+	email VARCHAR(64) UNIQUE NOT NULL,
+	expertise VARCHAR(64) NOT NULL,
+	address VARCHAR(255) UNIQUE NOT NULL);
+
+CREATE TABLE IF NOT EXISTS job_category(
+	name VARCHAR(64) PRIMARY KEY);
+
+CREATE TABLE IF NOT EXISTS cleaning(
+	name VARCHAR(64) PRIMARY KEY,
+	descrip VARCHAR(255),
+	price DEC(16, 2) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS pet_care(
+	name VARCHAR(64) PRIMARY KEY,
+	descrip VARCHAR(255),
+	price DEC(16, 2) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS tuition(
+	name VARCHAR(64) PRIMARY KEY,
+	descrip VARCHAR(255),
+	price DEC(16, 2) NOT NULL);
+
 CREATE TABLE IF NOT EXISTS transaction (
- customer_id VARCHAR(16) NOT NULL,
- provider_id VARCHAR(16) NOT NULL,
- customer_name VARCHAR(128) NOT NULL,
- provider_name VARCHAR(128) NOT NULL,
- address VARCHAR(255) UNIQUE NOT NULL,
- job VARCHAR(64) NOT NULL, 
- price DEC(16, 2) NOT NULL);
+	customer_id VARCHAR(16) NOT NULL,
+	provider_id VARCHAR(16) NOT NULL,
+	cust_address VARCHAR(255) UNIQUE NOT NULL,
+	expertise VARCHAR(64) NOT NULL, 
+	price DEC(16, 2) NOT NULL);
