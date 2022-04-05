c317156bf2aea9d190fd1089c64fe8684248402d
himzies <100895032+himzies@users.noreply.github.com>
2022-04-06 00:32:58 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -21,22 +21,22 @@ import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('database', app.views.database, name='database'),
-    path('database_provider', app.views.database_provider, name='database_provider'),
-    path('add', app.views.add, name='add'),
-    path('add_provider', app.views.add_provider, name='add_provider'),
-    path('view/<str:id>', app.views.view, name='view'),
-    path('edit/<str:id>', app.views.edit, name='edit'),
-    path('<str:id>/edit', app.views.edit, name='edit'),
-    path('view_provider/<str:id>', app.views.view_provider, name='view_provider'),
-    path('edit_provider/<str:id>', app.views.edit_provider, name='edit_provider'),    
+    path('database/', app.views.database, name='database'),
+    path('database_provider/', app.views.database_provider, name='database_provider'),
+    path('add/', app.views.add, name='add'),
+    path('add_provider/', app.views.add_provider, name='add_provider'),
+    path('view/<str:id>/', app.views.view, name='view'),
+    path('edit/<str:id>/', app.views.edit, name='edit'),
+    path('<str:id>/edit/', app.views.edit, name='edit'),
+    path('view_provider/<str:id>/', app.views.view_provider, name='view_provider'),
+    path('edit_provider/<str:id>/', app.views.edit_provider, name='edit_provider'),    
     path('', app.views.home, name='home'),
-    path('login', app.views.login, name='login'),
-    path('login_provider', app.views.login_provider, name='login_provider'),
-    path('<str:id>/services', app.views.services, name='services'),
-    path('<str:id>/job_cat/<str:service>', app.views.job_cat, name='job_cat'),
-    path('<str:id>/job_cat/<str:service>/job_req/<str:expertise>', app.views.job_req, name='job_req'),
-    path('test', app.views.test, name='test'),
+    path('login/', app.views.login, name='login'),
+    path('login_provider/', app.views.login_provider, name='login_provider'),
+    path('<str:id>/services/', app.views.services, name='services'),
+    path('<str:id>/job_cat/<str:service>/', app.views.job_cat, name='job_cat'),
+    path('<str:id>/job_cat/<str:service>/job_req/<str:expertise>/', app.views.job_req, name='job_req'),
+    path('test/', app.views.test, name='test'),
     #Testing for transaction
-    path('<str:id>/job_cat/<str:service>/job_req/<str:expertise>/transaction/<str:prov_id>', app.views.transaction, name='transaction')
+    path('<str:id>/job_cat/<str:service>/job_req/<str:expertise>/transaction/<str:prov_id>/', app.views.transaction, name='transaction')
 ]
